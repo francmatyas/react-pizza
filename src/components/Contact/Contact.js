8bebe27a365499e24252836d5524753eb46da733
Matyas Franc <franc.matyas@gmail.com>
2022-11-08 15:46:43 +0100

    cotact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,19 +1,49 @@
-import "./Contact.scss"
+import "./Contact.scss";
 
-import Banner from "../../assets/banner.png"
+import Banner from "../../assets/banner.png";
 
-function Contact () {
+import {
+  AiOutlineInstagram,
+  AiOutlineFacebook,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+
+function Contact() {
   return (
     <div className="contact">
-      <h1 className="contact__title">Contact Us</h1>
-      <img src={Banner} className="contact__banner"/>
-      <form>
-        <input className="contact__input" type="text" placeholder="Full name" />
-        <input className="contact__input" type="email" placeholder="E-mail" />
-        <textarea className="contact__input" type="text" placeholder="Message" />
-        <button className="contact__button">Contact Us</button>
-      </form>
+      <div className="contact__banner">
+        <img src={Banner} />
+      </div>
+
+      <div className="contact__content">
+        <h1 className="contact__title">Contact Us</h1>
+        <div className="contact__form">
+          <form>
+            <input
+              className="contact__input"
+              type="text"
+              placeholder="Full name"
+            />
+            <input
+              className="contact__input"
+              type="email"
+              placeholder="E-mail"
+            />
+            <textarea
+              className="contact__input"
+              type="text"
+              placeholder="Message"
+            />
+            <button className="contact__button">Contact Us</button>
+          </form>
+          <div className="contact__socials">
+            <AiOutlineInstagram className="contact__socials-icon" />
+            <AiOutlineFacebook className="contact__socials-icon" />
+            <AiOutlineTwitter className="contact__socials-icon" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-export default Contact
+export default Contact;
